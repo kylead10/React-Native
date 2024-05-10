3985c619eeec0dd55a048c73e28390c28be298bc
kylead10 <adiokayode111@gmail.com>
2024-05-11 00:35:25 +0100

    creating listElement

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/ListScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
+    List: ListScreen,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'List',
     defaultNavigationOptions: {
       title: 'App',
     },
